1a5d2eb52ffa25463dbbac2bf8dfef7a1094d021
Achintha Shiran <gpbandarakusal@gmail.com>
2023-01-26 13:17:18 +0530

    Arrow Shooting Trap created

--- Assets/Scripts/Traps/FireTrap.cs
+++ Assets/Scripts/Traps/FireTrap.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class FireTrap : MonoBehaviour
 {
+    private GameObject player;
+
+
     [Header("Fire Timers")]
     [SerializeField] private float activateDelay;
     [SerializeField] private float activeTime;
-    [SerializeField] private float damage;
+    [SerializeField] private float damage =10;
     private Animator anim;
     private SpriteRenderer spriteRend;
 
     private bool triggered; //when the trap gets triggered
     private bool active; // when the trap is active and can hurt the player
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
@@ -24,24 +28,30 @@ public class FireTrap : MonoBehaviour
         if(collision.tag == "Player")
         {
             if (!triggered)
-            {
-                //trigger the firetrap
-            }
+                StartCoroutine(ActivateFiretrap());
 
             if (active)
-                collision.GetComponent<PlayerHealth>().TakeDamage(damage);
+            Manager.instance.PlayerHealth.TakeDamage(damage);
         }
     }
 
     private IEnumerator ActivateFiretrap()
     {
+        //turn the sprite red to notify the player and trigger the trap
         triggered = true;
         spriteRend.color = Color.red;
+
+        //wait for dealy,activate trap, turn on animation, return color back to normal
         yield return new WaitForSeconds(activateDelay);
-        spriteRend.color = Color.white;
+        spriteRend.color = Color.white; // turn the sprite initial color back
+        
         active = true;
+        anim.SetBool("activated", true);
+
+        //wait until x seconds, deactivate trap and reset all variables and animator
         yield return new WaitForSeconds(activeTime);
         active = false;
         triggered = false;
+        anim.SetBool("activated", false);
     }
 }
